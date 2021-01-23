889b36a76d1513ee7830e31c6f60826b16640fc0
Aks1097 <76415695+Aks1097@users.noreply.github.com>
2021-01-23 12:05:56 -0700

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,24 @@
+class dustbin {
+    constructor(x, y) {
+      
+      this.width = 50;
+      this.height = 50;
+  
+      World.add(world, this.body);
+  //    var box1=createSprite(220,620,20,100);
+	//     box1.shapeColor="red";
+
+	// var box2=createSprite(330,660,200,20);
+	// box2.shapeColor="red";
+
+	// var box3=createSprite(440,620,20,100);
+  // box3.shapeColor="red";
+  
+    }
+    display(){
+      push();
+      fill("white");
+      pop();
+    }
+  };
+  
